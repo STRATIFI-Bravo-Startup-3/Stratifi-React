24d435145cf59dbd4e88c4cb3da12effb9756510
Ikenna Udemezue <iykethe1st@gmail.com>
2022-10-19 13:34:51 +0100

    Installed Tailwind

---
+++ stratifi-app/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  state = {};
+  render() {
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  }
+}
+
+export default NavBar;
