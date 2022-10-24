9c8a5ab4c8f99e999fd7fc9b4a27e1aac4a2eb19
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-24 18:15:24 +0100

    image part of the testimonial

--- stratifi-app/src/components/Testimonial.jsx
+++ stratifi-app/src/components/Testimonial.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import logo3 from "../images/logo3.svg";
+import img5 from "../images/img5.png";
+import img6 from "../images/img6.png";
+import img7 from "../images/img7.png";
+import img8 from "../images/img8.png";
 const Testimonial = () => {
   return (
     <section className="testimonial">
