9a8e7dabf736d5cfc847523c004ed76326f5a990
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-24 17:33:10 +0100

    image part of the testimonial

--- stratifi-app/src/components/Testimonial.jsx
+++ stratifi-app/src/components/Testimonial.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import logo3 from "../images/logo3.svg";
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <section className="testimonial">
+      <div className="justify-center items-center text-center py-4 px-3 container mx-auto">
+        <img src={logo3} alt="" className="w-1/2 justify-center items-center" />
+        <h2></h2>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonial;
