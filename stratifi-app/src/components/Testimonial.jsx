fc4cb5e42c1d2c36f0765f7594626e8501c87db7
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-24 18:25:46 +0100

    first part of the testimonial

--- stratifi-app/src/components/Testimonial.jsx
+++ stratifi-app/src/components/Testimonial.jsx
@@ -6,12 +6,35 @@ import img7 from "../images/img7.png";
 import img8 from "../images/img8.png";
 const Testimonial = () => {
   return (
-    <section className="testimonial">
+    <section className="testimonial  bg-purple-300">
       <div className="justify-center items-center text-center py-4 px-3 container mx-auto">
         <img src={logo3} alt="" className="w-1/2 block mx-auto" />
         <h2 className="font-semibold">
           View testimonials from some of our clients
         </h2>
+      </div>
+      <div className="mx-auto container flex flex-col  items-center gap-2 md:flex-row md:px-28 md:space-x-6 md:max-w-6xl ">
+        <div className="flex flex-col bg-purple-300 md:space-x-6 md:px-4 md:py-2">
+          <div className="flex flex-row   bg-purple-300 items-center mb-4">
+            <img src={img5} alt="" className="w-16 mr-5" />
+            <h2 className="text-sm items-center ">Michelle Ikeji</h2>
+          </div>
+          <p className="px-8">
+            simple and effective. no long hours searching for reliable
+            influencers.
+          </p>
+        </div>
+
+        <div className="flex flex-col bg-purple-300 md:space-x-6 md:px-4 md:py-2">
+          <div className="flex flex-row   bg-purple-300 items-center mb-4">
+            <img src={img6} alt="" className="w-16 mr-5" />
+            <h2 className="text-sm items-center text-center">Ovie Boco</h2>
+          </div>
+          <p class="px-">
+            An absolute influencer and brand marketing tool, wow all in one
+          </p>
+        </div>
+        {/* second Testimonial */}
       </div>
     </section>
   );
