5c2054a43511242251ec0db46700cead6e0b9fc7
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-27 13:54:23 +0100

    testimonial

--- stratifi-app/src/components/Testimonial.jsx
+++ stratifi-app/src/components/Testimonial.jsx
@@ -8,16 +8,16 @@ const Testimonial = () => {
   return (
     <section>
       {/* <!-- Container to heading and testm blocks --> */}
-      <div class="max-w-6xl px-5 mx-auto mt-32 text-center">
+      <div class="max-w-6xl px-5 mx-auto mt-10 text-center">
         {/* <!-- Heading --> */}
         <h2 class="text-4xl font-bold text-center">
           Testimonials from our clients
         </h2>
         {/* <!-- Testimonials Container --> */}
-        <div class="flex flex-col mt-32 md:flex-row md:space-x-6">
+        <div class="flex flex-col mt-10 md:flex-row md:space-x-6">
           {/* <!-- Testimonial 1 --> */}
           <div class="flex flex-col  mb-4  rounded-lg  p-4 space-y-20 md:mb-32 shadow-sm md:w-1/3 md:shadow-md">
-            <p class="text-sm ">
+            <p class="text-sm font-semi-bold mb-10">
               An absolute influencer marketing tool. Effective and trustworthy.
             </p>
             <div className="flex items-center ">
@@ -41,7 +41,7 @@ const Testimonial = () => {
 
           {/* <!-- Testimonial 3 --> */}
           <div class="flex flex-col  space-y-20  p-4 rounded-lg mb-4 md:mb-32  shadow-sm md:w-1/3 md:shadow-md">
-            <p class="text-sm  ">
+            <p class="text-sm  mb-10 ">
               Simple and effective. no long hours searching for reliable
               influencers.
             </p>
