97202786a250ba142e7564f3b552ecbd5404ccdd
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-27 14:00:42 +0100

    image update

--- stratifi-app/src/components/Testimonial.jsx
+++ stratifi-app/src/components/Testimonial.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import logo3 from "../images/logo3.svg";
+
 import img5 from "../images/img5.png";
 import img6 from "../images/img6.png";
-import img7 from "../images/img7.png";
+
 import img8 from "../images/img8.png";
 const Testimonial = () => {
   return (
