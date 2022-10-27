c6de32c9cd27d15c839faef3af3ec722711b0a62
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-27 12:53:02 +0100

    testimnial heading

--- stratifi-app/src/components/Testimonial.jsx
+++ stratifi-app/src/components/Testimonial.jsx
@@ -8,7 +8,12 @@ const Testimonial = () => {
   return (
     <section>
       {/* <!-- Container to heading and testm blocks --> */}
-      <div class="max-w-6xl px-5 mx-auto mt-32 text-center"></div>
+      <div class="max-w-6xl px-5 mx-auto mt-32 text-center">
+        {/* <!-- Heading --> */}
+        <h2 class="text-4xl font-bold text-center">
+          Testimonials from our clients
+        </h2>
+      </div>
     </section>
   );
 };
