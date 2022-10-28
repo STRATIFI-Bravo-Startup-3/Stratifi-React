275fc61e887558d2eec822d35ed1ca853831c1ef
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-28 15:58:48 +0100

    influence

--- stratifi-app/src/pages/Influencers.jsx
+++ stratifi-app/src/pages/Influencers.jsx
@@ -17,6 +17,13 @@ const Influencers = () => {
             <div className="shadow-sm bg-gray-200"></div>
             <p className="text-sm">House of thera</p>
           </div>
+          <div></div>
+          <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Button
+          </button>
+          <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Button
+          </button>
         </div>
       </div>
     </section>
