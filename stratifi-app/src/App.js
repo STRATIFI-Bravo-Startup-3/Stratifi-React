db0b9cb5ddb044c243400ead7bac83948a7dbb3b
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-28 16:09:30 +0100

    App

--- stratifi-app/src/App.js
+++ stratifi-app/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Influencers from "./pages/Influencers";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/influencers" element={<Influencers />} />
         </Routes>
       </Router>
     </div>
