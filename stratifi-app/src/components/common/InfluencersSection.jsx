85c62205ba2ec33e9ece55cc16a2f8fa19889e99
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-31 19:47:49 +0100

    section

---
+++ stratifi-app/src/components/common/InfluencersSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InfluencersSection = () => {
+  return (
+    <section>
+      <div className="bg-purple-300"></div>
+    </section>
+  );
+};
+
+export default InfluencersSection;
