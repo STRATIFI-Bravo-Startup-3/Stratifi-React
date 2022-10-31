8fed97d56ddf33980dc5a8eab64f805a70b19016
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-31 20:40:36 +0100

    section

--- stratifi-app/src/components/common/InfluencersSection.jsx
+++ stratifi-app/src/components/common/InfluencersSection.jsx
@@ -5,7 +5,7 @@ const InfluencersSection = () => {
     <section className="bg-purple-300">
       <div className=" mx-auto container px-10 py-10">
         <div className="flex items-center">
-          <div className="w-56 h-56 bg-gray-500 rounded-full mr-4 m-4"></div>
+          <div className="w-56 h-56 bg-gray-500 rounded-full mr-4 font-semibold"></div>
           <p>
             Hi, <br />
             Anna
