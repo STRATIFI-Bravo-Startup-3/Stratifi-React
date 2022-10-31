80434ac710292c9e2a753cd5b332b9f15dedb7b4
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-10-31 20:39:05 +0100

    section

--- stratifi-app/src/components/common/InfluencersSection.jsx
+++ stratifi-app/src/components/common/InfluencersSection.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const InfluencersSection = () => {
   return (
-    <section>
-      <div className="bg-purple-300"></div>
+    <section className="bg-purple-300">
+      <div className=" mx-auto container px-10 py-10">
+        <div className="flex items-center">
+          <div className="w-56 h-56 bg-gray-500 rounded-full mr-4 m-4"></div>
+          <p>
+            Hi, <br />
+            Anna
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
