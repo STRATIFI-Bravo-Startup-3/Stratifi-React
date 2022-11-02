9102d03a5a342165a1e3c8e9221fa973952d2283
Iyke U <iykethe1st@gmail.com>
2022-11-02 13:41:12 +0100

    Revert "Branch registration form component"

--- stratifi-app/src/App.js
+++ stratifi-app/src/App.js
@@ -9,7 +9,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Influencers from "./pages/Influencers";
 // import SubscribeContent from "./pages/SubscribeContent";
 import Mailchimp from "./pages/SubscribeContent";
-import Brandreg from "./pages/Brandreg";
 
 function App() {
   return (
@@ -22,7 +21,6 @@ function App() {
           <Route path="/influencers" element={<Influencers />} />
           <Route path="/get-content" element={<Mailchimp />} />
           <Route path="/update-profile" element={<UpdateProfile />} />
-          <Route path="/brand-registration" element={<Brandreg />} />
         </Routes>
       </Router>
     </div>
