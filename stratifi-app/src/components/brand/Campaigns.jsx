02a304c6d0e3903a8df954b3c2e487b71c00bcd5
Ikenna <iykethe1st@gmail.com>
2023-04-10 13:27:23 +0100

    fixing overflow to scroll on Campaigns

--- stratifi-app/src/components/brand/Campaigns.jsx
+++ stratifi-app/src/components/brand/Campaigns.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Campaigns = () => {
   return (
-    <section className="flex flex-col w-80 overflow-x-scroll">
+    <section className="flex flex-col w-full overflow-x-scroll overflow-hidden">
       <h1 className="font-bold text-gray-600 text-lg px-4">Campaigns</h1>
-      <div className="bg-gray-200 rounded-lg flex items-center gap-4 justify-around py-8 overflow-x-scroll w-full">
+      <div className="bg-gray-200 rounded-lg flex items-center gap-4 justify-around py-8 overflow-hidden overflow-x-scroll">
         <div className="rounded flex flex-col items-center ring-1 w-[10rem] gap-4 text-sm bg-gray-100 p-2 ring-gray-300 shadow-sm">
           <h1 className="font-bold">MaraCampaign</h1>
           <div className="flex flex-col items-center">
