f00f8f60dbf7d78b1ad4bc505993777c274c6592
Ikenna <iykethe1st@gmail.com>
2023-05-06 18:52:23 +0100

    Added invitation to influencer dashboard

--- stratifi-app/src/components/influencer/Dashboard.jsx
+++ stratifi-app/src/components/influencer/Dashboard.jsx
@@ -2,7 +2,7 @@ import { HiCheckBadge } from "react-icons/hi2";
 import ProfileCover from "./ProfileCover";
 import { IoMailUnread, IoWallet } from "react-icons/io5";
 import { HiSpeakerphone } from "react-icons/hi";
-import { BsPersonCircle } from "react-icons/bs";
+import { BsBookmarkPlusFill, BsPersonCircle } from "react-icons/bs";
 
 const Dashboard = () => {
   return (
@@ -25,6 +25,15 @@ const Dashboard = () => {
           </div>
           <h1 className="flex-end">6 Active</h1>
         </div>
+
+        <div className="w-[10rem] h-[10rem] sm:w-[15rem] sm:h-[10rem] p-6 bg-[#F9E5FF] rounded-lg flex flex-col justify-between hover:shadow-md active:shadow-inner">
+          <div className="flex items-center gap-2">
+            <BsBookmarkPlusFill className="w-5 h-5" />
+            <h1 className="font-bold">Invitation(s)</h1>
+          </div>
+          <h1 className="flex-end">2 New</h1>
+        </div>
+
         <div className="w-[10rem] h-[10rem] sm:w-[15rem] sm:h-[10rem] p-6 bg-[#F9E5FF] rounded-lg flex flex-col justify-between hover:shadow-md active:shadow-inner">
           <div className="flex items-center gap-2">
             <IoWallet className="w-5 h-5" />
